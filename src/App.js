376b59907ac4d42f63c9a54b292a20f9f00294bd
Miryam <mbathilde@gmail.com>
2021-10-05 12:07:18 +0200

    componentes de clase y de funciones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+//import Greeting from "./components/pure/greeting";
+import Greetingf from "./components/pure/greetingF";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo
-        </p>
+        {/* Importamos el Componente propio Greeting.jsx y lo renderizamos*/}
+        {/*le pasamos la propiedad name*/}
+        {/* <Greeting name="Miryam"></Greeting> */}
+        <Greetingf name="Miryam"></Greetingf>
       </header>
     </div>
   );
